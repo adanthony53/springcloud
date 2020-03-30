669b887110753f152eb77e4bd33fdc05967bfb77
adanthony53 <32273951+adanthony53@users.noreply.github.com>
2020-03-30 09:08:12 -0700

    added Feign

---
+++ feign/src/main/java/com/example/feign/impl/FeignError.java
@@ -0,0 +1,21 @@
+package com.example.feign.impl;
+
+import com.example.entity.Student;
+import com.example.feign.FeignProviderClient;
+import org.springframework.stereotype.Component;
+
+import java.util.Collection;
+
+@Component
+public class FeignError implements FeignProviderClient {
+
+    @Override
+    public Collection<Student> findAll() {
+        return null;
+    }
+
+    @Override
+    public String index() {
+        return "server error";
+    }
+}
