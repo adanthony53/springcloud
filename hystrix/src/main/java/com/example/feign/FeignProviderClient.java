8cc1540bf5665b79d6cf642d7134808874b44d38
adanthony53 <32273951+adanthony53@users.noreply.github.com>
2020-03-30 10:28:05 -0700

    added Hystrix

--- hystrix/src/main/java/com/example/feign/FeignProviderClient.java
+++ hystrix/src/main/java/com/example/feign/FeignProviderClient.java
@@ -1,14 +1,12 @@
 package com.example.feign;
 
 import com.example.entity.Student;
-import com.example.feign.impl.FeignError;
 import org.springframework.cloud.openfeign.FeignClient;
 import org.springframework.web.bind.annotation.GetMapping;
 
 import java.util.Collection;
 
 @FeignClient(value = "provider")
-//@FeignClient(value = "provider", fallback = FeignError.class)
 public interface FeignProviderClient {
     @GetMapping("/student/findAll")
     public Collection<Student> findAll();
